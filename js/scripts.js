1cd3848e7725190318be9f7d1155348ea4977a21
Charles Carter and Michael Lor <charles.h.carter@gmail.com>
2016-01-26 13:56:33 -0800

    add functionality for applying math to ticketPrice

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
-function Ticket(ticketPrice) {
+function Ticket(ticketNum) {
+  this.ticketNum = ticketNum;
+};
+
+function Price(ticketPrice) {
   this.ticketPrice = [ticketPrice];
 };
-//
-// Ticket.prototype.ticketMatinee = function() {
-//   return //this ticketprice -2
-// }
-//
+
+
+
+Price.prototype.ticketMatinee = function() {
+  this.ticketPrice = this.ticketPrice - 2;
+  return this.ticketPrice;
+};
+
+Price.prototype.ticketSenior = function() {
+  this.ticketPrice = this.ticketPrice/2;
+  return this.ticketPrice;
+};
+
 // Ticket.prototype.ticketSenior = function() {
 //   return //this ticket price /2
 // }
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,23 @@
 describe('Ticket', function() {
-  it("will put in a price of a ticket.", function() {
-    var testTicket = new Ticket (5);
-    expect(testTicket.ticketPrice).to.eql([5]);
+  it("will make a ticket object.", function() {
+    var testTicket = new Ticket ("ticket1");
+    expect(testTicket.ticketNum).to.equal("ticket1");
   });
+});
+
+describe('Price', function() {
+  it("will put in a price of a ticket.", function() {
+    var testPrice = new Price (5);
+    expect(testPrice.ticketPrice).to.eql([5]);
+  });
+
+  it("will subtract ticketPrice by 2.", function() {
+    var testPrice = new Price (5);
+    expect(testPrice.ticketMatinee()).to.equal(3);
+  });
+
+  it("will divide ticketPrice by 2.", function() {
+    var testPrice = new Price (10);
+    expect(testPrice.ticketSenior()).to.equal(5);
+  })
 });
